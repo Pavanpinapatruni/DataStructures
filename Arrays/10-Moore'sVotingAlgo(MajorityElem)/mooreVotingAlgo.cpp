f962debc65d795fdf2057929213e30d22c5021e7
Pavanpinapatruni <Pinapatruni.VenkataChandraPavan@in.bosch.com>
2025-09-29 12:32:39 +0530

    Moore's Voting Algorithm to find Majority element more than n/2 times

--- Arrays/10-Moore'sVotingAlgo(MajorityElem)/mooreVotingAlgo.cpp
+++ Arrays/10-Moore'sVotingAlgo(MajorityElem)/mooreVotingAlgo.cpp
@@ -2,6 +2,32 @@
 using namespace std;
 
 
+
+/*
+link : https://leetcode.com/problems/majority-element/description/""
+Given an array nums of size n, return the majority element.
+
+The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+
+ 
+
+Example 1:
+
+Input: nums = [3,2,3]
+Output: 3
+Example 2:
+
+Input: nums = [2,2,1,1,1,2,2]
+Output: 2
+ 
+
+Constraints:
+
+n == nums.length
+1 <= n <= 5 * 104
+-109 <= nums[i] <= 109
+*/
+
 // better way would be hashing the below one is optimal
 class Solution {
 public:
